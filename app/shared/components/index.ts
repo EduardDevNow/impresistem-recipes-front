edc2b2f8170145cbff514d60739598d7a97d09b8
Nelson Arevalo <nelsonarevalo0696@gmail.com>
2024-09-10 16:18:41 -0500

    Rest code

--- app/shared/components/index.ts
+++ app/shared/components/index.ts
@@ -1,10 +1,12 @@
 import dynamic from 'next/dynamic'
 
 const Navbar = dynamic(() => import('./Navbar'));
+const PageTitle = dynamic(() => import('./PageTitle'));
 
 const useSharedComponents = () => {
     return {
-        Navbar
+        Navbar,
+        PageTitle
     }
 }
 
